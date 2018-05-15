800df271b57b8034e8d9893058269245ca9d80df
Andrey Berezin <aberezin@natera.com>
2018-05-15 11:26:27 +0300

    Init commit.

---
+++ src/main/java/org/aber/tfb/queue/tfbqueue/TfbQueueApplication.java
@@ -0,0 +1,12 @@
+package org.aber.tfb.queue.tfbqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfbQueueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TfbQueueApplication.class, args);
+    }
+}
---
+++ src/test/java/org/aber/tfb/queue/tfbqueue/TfbQueueApplicationTests.java
@@ -0,0 +1,16 @@
+package org.aber.tfb.queue.tfbqueue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TfbQueueApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
